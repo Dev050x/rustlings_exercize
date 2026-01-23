15be404fac3ef32842260e3198f045fdf95b1ab8
0xDev05 <divpatel1274@gmail.com>
2026-01-24 02:05:01 +0530

    complete some exercize

--- exercises/18_iterators/iterators3.rs
+++ exercises/18_iterators/iterators3.rs
@@ -1,3 +1,5 @@
+use std::ops::Div;
+
 #[derive(Debug, PartialEq, Eq)]
 enum DivisionError {
     // Example: 42 / 0
@@ -11,21 +13,31 @@ enum DivisionError {
 // TODO: Calculate `a` divided by `b` if `a` is evenly divisible by `b`.
 // Otherwise, return a suitable error.
 fn divide(a: i64, b: i64) -> Result<i64, DivisionError> {
-    todo!();
+    if b == 0 {
+        Err(DivisionError::DivideByZero)
+    } else if a == i64::MIN && b == -1 {
+        Err(DivisionError::IntegerOverflow)
+    }
+    else if a % b == 0  {
+        Ok(a/b)
+    }else {
+        Err(DivisionError::NotDivisible)
+    }
+
 }
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `Ok([1, 11, 1426, 3])`
-fn result_with_list() {
+fn result_with_list() -> Result<Vec<i64>, DivisionError> {
     let numbers = [27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    Ok(numbers.into_iter().map(|n| divide(n, 27).unwrap()).collect())
 }
 
 // TODO: Add the correct return type and complete the function body.
 // Desired output: `[Ok(1), Ok(11), Ok(1426), Ok(3)]`
-fn list_of_results() {
+fn list_of_results() -> Result<Vec<Result<i64, DivisionError>> , DivisionError> {
     let numbers = [27, 297, 38502, 81];
-    let division_results = numbers.into_iter().map(|n| divide(n, 27));
+    Ok(numbers.into_iter().map(|n| divide(n, 27)).collect())
 }
 
 fn main() {
@@ -70,6 +82,6 @@ mod tests {
 
     #[test]
     fn test_list_of_results() {
-        assert_eq!(list_of_results(), [Ok(1), Ok(11), Ok(1426), Ok(3)]);
+        assert_eq!(list_of_results().unwrap(), [Ok(1), Ok(11), Ok(1426), Ok(3)]);
     }
 }
